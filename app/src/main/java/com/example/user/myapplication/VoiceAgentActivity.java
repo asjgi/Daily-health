ecbc4db3c1a0f2d3a38333477b20f245418d0c06
David.Ahn <asjgi@naver.com>
2018-12-24 19:35:32 +0900

    Add to about new voice agent tab2
    Add to about new voice agent tab2

--- app/src/main/java/com/example/user/myapplication/VoiceAgentActivity.java
+++ app/src/main/java/com/example/user/myapplication/VoiceAgentActivity.java
@@ -1,5 +1,8 @@
 package com.example.user.myapplication;
 
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,21 +17,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class VoiceAgentActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_voice_agent);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -74,32 +77,25 @@ public class MainActivity extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+        @Override
+        public boolean onNavigationItemSelected (@NonNull MenuItem menuItem){
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-            Intent intent = new Intent(MainActivity.this, CameraActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_gallery) {
-            Intent intent = new Intent(MainActivity.this, GallaryActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_slideshow) {
-            Intent intent = new Intent(MainActivity.this, VoiceAgentActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_manage) {
+            int id = menuItem.getItemId();
+            if (id == R.id.nav_camera) {
+                // Handle the camera action
+                Intent intent = new Intent(VoiceAgentActivity.this, CameraActivity.class);
+                startActivity(intent);
+            } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_share) {
+            } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_send) {
+            } else if (id == R.id.nav_share) {
 
+            } else if (id == R.id.nav_send) {
+
+            }
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
-}
